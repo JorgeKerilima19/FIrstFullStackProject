4872f3445fe2837bf2c457269f32658e7c0bf2b4
JorgeMioska <quispechilojorge@gmail.com>
2023-09-03 04:46:39 -0500

    HomeBanner separated + homepage padding reworked

--- src/components/homepage/Features.tsx
+++ src/components/homepage/Features.tsx
@@ -26,8 +26,8 @@ const features = [
 export const Features = () => {
   return (
     <div className="flex flex-col gap-20">
-      <div className="flex flex-wrap">
-        <h3 className="text-4xl pr-20 font-medium self-start pb-10">
+      <div className="pl-10 flex flex-wrap gap-16">
+        <h3 className="text-4xl pr-5 font-medium self-start">
           Why Choose Us?
         </h3>
         <p className="text-lg font-light w-full md:w-3/5 lg:w-3/5">
@@ -38,8 +38,8 @@ export const Features = () => {
           diam. Proin eget tortor venenatis ex consectetur blandit.{" "}
         </p>
       </div>
-      <div className="grid gap-10 xl:grid-cols-2">
-        <div className="grid lg:grid-cols-2 gap-20">
+      <div className="pl-10 grid gap-10 xl:grid-cols-2 bg-gray-100">
+        <div className="grid lg:grid-cols-2 gap-20 py-16">
           {features.map((feature, index) => (
             <Card key={index} feature={feature} />
           ))}
--- src/routes/HomePage.tsx
+++ src/routes/HomePage.tsx
@@ -1,34 +1,13 @@
-import TypewriterComponent from "typewriter-effect";
-import { Navbar, Features, AboutHome } from "../components/homepage";
+import { Features, AboutHome, HomeBanner } from "../components/homepage";
 import { Clients, Invitation } from "../components";
-
-import img from "../assets/images/banner.jpg";
 
 export const HomePage = () => {
   return (
     <>
-      <section className="pl-10 relative min-h-vh flex flex-col justify-center gap-12">
-        <div className="banner absolute w-full h-full -z-10">
-          <img
-            className="w-full h-full object-cover absolute"
-            src={img}
-            alt=""
-          />
-        </div>
-        <div className="flex text-7xl gap-2">
-          <span>Info for those </span>
-          <TypewriterComponent
-            options={{
-              strings: ["Smart", "Worried"],
-              autoStart: true,
-              loop: true,
-              wrapperClassName: "text-red-800",
-            }}
-          />
-        </div>
-        <Navbar />
+      <section className="relative min-h-vh flex flex-col justify-center gap-12">
+        <HomeBanner />
       </section>
-      <section className="pl-10 relative min-h-vh flex flex-col justify-center gap-12 pt-20 bg-gray-100">
+      <section className="relative min-h-vh flex flex-col justify-center gap-12 pt-20">
         <Features />
       </section>
       <section className="pl-10 flex gap-12 flex-wrap py-20 text-lg">
