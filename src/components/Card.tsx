c206e635c02c5c5eb48378afc8a17312b2c86930
JorgeMioska <quispechilojorge@gmail.com>
2023-09-04 00:22:03 -0500

    card hover

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,7 +4,7 @@ import { ChildProps } from "./Interfaces";
 
 export const Card = ({ feature }: ChildProps) => {
   return (
-    <article className="w-full sm:w-80 bg-gray-200 relative flex flex-col items-start gap-5 p-5 rounded-lg">
+    <article className="w-full sm:w-80 bg-gray-200 relative flex flex-col items-start gap-5 p-5 rounded-lg hover:transform hover:translate-y-[-2%] transition duration-300 ">
       <img src={feature.img} alt="" />
       <header className="text-2xl font-semibold">{feature.title}</header>
       <span className="text-lg font-normal">{feature.description}</span>
