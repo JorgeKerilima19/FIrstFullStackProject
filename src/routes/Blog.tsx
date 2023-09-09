f5148c5ff439ced786ef0ce9de07e95ee9709dc1
JorgeMioska <quispechilojorge@gmail.com>
2023-09-08 23:46:37 -0500

    Blog responsive

--- src/routes/Blog.tsx
+++ src/routes/Blog.tsx
@@ -6,9 +6,13 @@ export const Blog = () => {
       <h2 className="text-4xl font-bold">Blog</h2>
       <div className="py-10 grid justify-center gap-y-5">
         {posts.map((post) => (
-          <article className="cont grid grid-cols-3 gap-5 border-b-2 py-2">
-            <img src={post.img} alt="" className="h-32 w-60 object-cover" />
-            <div className="flex flex-col gap-2 col-span-2">
+          <article className="cont grid grid-cols-1 md:grid-cols-3 gap-5 border-b-2 py-5 place-items-center md:place-items-stretch ">
+            <img
+              src={post.img}
+              alt=""
+              className="cols-span-2 h-48 w-128 md:h-32 md:w-60 object-cover"
+            />
+            <div className="flex flex-col gap-2 md:col-span-2">
               <header className="title text-xl font-semibold transition duration-300">
                 {post.title}
               </header>
